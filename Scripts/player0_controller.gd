0a87d92b405719bb2244e6e01d90093f1c735b3a
mariaannae <maria.edwards@gmail.com>
2025-10-02 09:17:46 -0400

    adding jump buffer, acceleration and friction to player physics

--- Scripts/player0_controller.gd
+++ Scripts/player0_controller.gd
@@ -5,7 +5,8 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 const ACCELERATION = 2000.0
 const FRICTION = 2000.0
-const COYOTE_TIME = 0.1
+const COYOTE_TIME = 0.1 # coyote timer is a nice little buffer when the player jumps to make it easier to not fall
+#i guess this stuff is standard for platformers according to several tutorials?
 const JUMP_BUFFER_TIME = 0.1
 
 # Movement state
@@ -27,21 +28,42 @@ func _physics_process(delta):
 	# Add gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	# Update coyote time - start timer when leaving ground without jumping
+	if was_on_floor and not is_on_floor() and velocity.y >= 0:
+		coyote_timer = COYOTE_TIME
 	
-	# Handle jump
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Update jump buffer - remember jump input
+	if Input.is_action_just_pressed("ui_accept"):
+		jump_buffer_timer = JUMP_BUFFER_TIME
+	
+	# Handle jump with coyote time and buffer
+	if jump_buffer_timer > 0 and (is_on_floor() or coyote_timer > 0):
 		velocity.y = JUMP_VELOCITY
+		jump_buffer_timer = 0
+		coyote_timer = 0
 	
 	# Get input direction for horizontal movement
 	var direction = Input.get_axis("ui_left", "ui_right")
 	print(direction)
 	
 	if direction != 0:
-		velocity.x = direction * SPEED
+		# Use acceleration for smoother movement
+		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION * delta)
 		# Flip sprite based on direction
 		$AnimatedSprite2D.flip_h = direction < 0
-		# TODO: dif. sprite when it's still.
+		# TODO: dif. animation when it's still vs moving (maybe vs jumping)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Use friction for smoother stopping
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+		
+	# Update timers
+	if coyote_timer > 0:
+		coyote_timer -= delta
+	if jump_buffer_timer > 0:
+		jump_buffer_timer -= delta
+	
+	# Store floor state for next frame
+	was_on_floor = is_on_floor()
 	
 	move_and_slide()
